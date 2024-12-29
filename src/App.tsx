0487cd1a953a6241ddd3dc4e861d2e7671db2e22
Jason Rustom <jasonrustom04@gmail.com>
2024-12-29 13:06:37 -0500

    added comment about how to continue with developing the site

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,10 @@ function App() {
 
   return (
     <Router>
+        {/*
+        Sine the site is a scroll and not different pages, can just put a global component here which has all the other sections in a justify-content vertical, and can put header above it
+        and then put routes after that, routes doesn't matter where it is. and then clicking on navlink should scroll instead of changing pages (scrolling manually should also update url)
+        */}
         <Header/>
 
 

f0884ea95f6ac40e1ca7bad0b6060dc5b466b6bc
Jason Rustom <jasonrustom04@gmail.com>
2024-12-29 13:04:20 -0500

    added routes - need to actually download dependency though

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from '../../../Tennis Website/interclub/node_modules/react-router-dom';
+
 
 import Header from './Components/header.tsx';
+import Home from './Components/home.tsx';
+import ResumePage from './Components/resumePage.tsx';
+
+import './App.css';
 
 
 function App() {
 
   return (
-    <>
+    <Router>
         <Header/>
-    </>
+
+
+        <Routes>
+            <Route path={'/home'} element={<Home/>}/>
+
+            <Route path={'/resume/'} element={<ResumePage/>}/>
+        </Routes>
+
+    {/*    Check how to make the default url /home instead of / */}
+    </Router>
   )
 }
 

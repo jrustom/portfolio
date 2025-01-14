4cd13ba9e7092f7a39e99e244d8d01f37d953360
Jason Rustom <jasonrustom04@gmail.com>
2025-01-13 20:59:31 -0500

    added landing page - complete redesign

---
+++ src/Components/landingPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './landingPage.css'
+
+const LandingPage: React.FC = () => {
+
+
+    return (
+        <div className="landingPageBackground">
+          <div className={"landingPageContainer"}>
+              JR.
+          </div>
+        </div>
+    );
+}
+
+export default LandingPage;
